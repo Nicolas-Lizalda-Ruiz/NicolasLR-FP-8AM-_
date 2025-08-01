29fa944eca002f93469b9f83c8bbed2ebe4022a9
Nicolas Lizalda Ruiz <nicolas.lizalda@upb.edu.co>
2025-08-01 09:30:20 -0500

    Variables, constantes y tipos de datos similares

--- 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -10,6 +10,18 @@ namespace _1.InicioVisualStudio
     {
         static void Main(string[] args)
         {
+            string peepefart = "woooouuuooouuuuaaaaaaooooouuuwuuuaaaaaoooaaauuuwooooaaauuuhh story of undertale"; 
+            const string pi = "3.14592653";
+            string lyricsPart1 = "i fell from the light";
+            //-    -    -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+
+            Console.Write("piiiipipiiiii\n");
+            //peepefart = "poopyyfarf";
+            //pi = "3.1416"; no puedo acher nada (ekisdent)
+
+            Console.Write("\t" + peepefart + " " + lyricsPart1 + " " + pi);
+
+            Console.ReadKey();
         }
     }
 }
