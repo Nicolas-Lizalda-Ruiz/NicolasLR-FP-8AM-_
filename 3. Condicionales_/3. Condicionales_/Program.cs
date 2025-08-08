a6cb501b7ca78c90f63e0bfa36cf1488e6946e1d
Nicolas Lizalda Ruiz <nicolas.lizalda@upb.edu.co>
2025-08-08 09:35:08 -0500

    Termine la tarea xd

--- 3. Condicionales_/3. Condicionales_/Program.cs
+++ 3. Condicionales_/3. Condicionales_/Program.cs
@@ -10,29 +10,49 @@ namespace _3.Condicionales_
     {
         static void Main(string[] args)
         {
+            /*
             //Condicionales simples
-            string nombrePersona = "";
+            string nombrePersona1 = "";
             float sueldo = 0;
 
             Console.WriteLine("Ingrese su nombre, usuario: ");
-            nombrePersona = Console.ReadLine(); //lo que se ponga en consola se lee como un string
+            nombrePersona1 = Console.ReadLine(); //lo que se ponga en consola se lee como un string
 
             Console.WriteLine("Ingrese su sueldo, usuario; ");
             sueldo = Convert.ToInt32(Console.ReadLine());
 
             if (sueldo > 3000)
             {
-                Console.WriteLine($"usuario {nombrePersona}, tu sueldo es mayor a 3000, por ende, debes abonar impuestos.");
+                Console.WriteLine($"usuario {nombrePersona1}, tu sueldo es mayor a 3000, por ende, debes abonar impuestos.");
             }
 
             else
             {
-                Console.WriteLine($"usuario {nombrePersona}, tu sueldo es menor que 3000, por ende, no debes abonar impuestos");
+                Console.WriteLine($"usuario {nombrePersona1}, tu sueldo es menor que 3000, por ende, no debes abonar impuestos");
             }
-
+            */
             Console.WriteLine("----------------------------------------------");
             //2. Crea un algoritmo que lea la edad de un usuario, si el usuario es mayor de 18, se debe mostrar el siguiente mensaje: “Bienvenido a mi sitio web”. 
 
+            float edad = 0;
+
+            Console.WriteLine("Ingrese su edad antes de entrar al sitio: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+
+            if (edad < 18)
+            {
+                Console.WriteLine("No estas permitido a entrar al sitio, eres menor de edad");
+            }
+
+            else if(edad > 18 && edad < 100)
+            {
+                Console.WriteLine("Bienvenido a mi sitio web");
+            }
+
+            else
+            {
+                Console.WriteLine("ingresa una edad válida porfavor");
+            }
         }
     }
 }
