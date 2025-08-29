fca1f21df16cb822b682eb79619a3eaed1274042
Nicolas Lizalda Ruiz <nicolas.lizalda@upb.edu.co>
2025-08-29 08:53:40 -0500

    Ejemplo de while loops

---
+++ 9. Ciclo while/9. Ciclo while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Ciclo_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} | Acumulador: {acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine("La suma de los primeros 5 números enteros es: " + acumulador);
+        }
+
+    }
+}
