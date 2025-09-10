fc480dc79ce2d8d08ee0ed8a20b6708595eff667
Nicolas Lizalda Ruiz <nicolas.lizalda@upb.edu.co>
2025-09-10 08:54:55 -0500

    Terminé el tercer ejercicio de do while loops con espacio de error

---
+++ 18. Do while loops 2/18. Do while loops 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Do_while_loops_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Algoritmo que le pida a un grupo de usuarios su nombre, número de cuenta y saldo; para cada usuario se debe hacer lo siguiente: 
+                Si el saldo es mayor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “Es apto para el crédito” 
+
+                Si el saldo es menor a 3’000.000, sacar un mensaje que diga el nombre del usuario, el número de cuenta, el saldo y “No es apto para el crédito” 
+
+                El algoritmo debe permitir preguntar la información de usuarios, hasta que se le indique que ya no se desea preguntar más. 
+
+                Además, el algoritmo debe permitir mostrar el número de usuarios a los que se le pregunto por la información, y debe mostrar el promedio de los saldos
+             */
+
+            string nombreDeUsuario = "";
+
+            Console.WriteLine("Usuario, digita tu nombre, número de cuenta y tu saldo. Al final se preguntará por el turno de otro usuario si estas en grupo. Ya cuando termines tu turno se mostrará");
+        }
+    }
+}
---
+++ 19. Do while (3)/19. Do while (3)/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Do_while__3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            int cantidadHombres = 0;
+            int cantidadMujeres = 0;
+            int menorDeEdad = 0;
+            int mayorDeEdad = 0;
+            char generoDePersonas = ' ';
+
+            int preguntasHechasEnTotal = 0;
+
+            do
+            {
+                preguntasHechasEnTotal++;
+
+                Console.WriteLine("-----------------------------------------------------------------------------");
+                Console.WriteLine($"Estudiante #{preguntasHechasEnTotal}, porfavor ingresa tu edad: ");
+                edad = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("-----------------------------------------------------------------------------");
+
+                Console.WriteLine($"Estudiante #{preguntasHechasEnTotal}, porfavor ingresa tu género. Debes poner 'H' para hombre o 'M' para mujer. Ponlo en mayúscula: ");
+                generoDePersonas = char.Parse(Console.ReadLine());
+
+                if (edad >= 18 && edad < 100)
+                {
+                    mayorDeEdad++;
+                }
+                else if (edad < 18 && edad > 5)
+                {
+                    menorDeEdad++;
+                }
+                else
+                {
+                    Console.WriteLine("Ingresa una edad válida porfavor. El sistema reinicia para tus datos");
+                    preguntasHechasEnTotal--;
+                }
+
+                if (generoDePersonas == 'M')
+                    cantidadMujeres++;
+                else if (generoDePersonas == 'H')
+                    cantidadHombres++;
+                else
+                {
+                    Console.WriteLine("Ingresa un género válido porfavor, El sistema reinicia para tus datos");
+                    if (edad >= 5 && edad <= 100)
+                    {
+                        preguntasHechasEnTotal--;
+                    }
+
+                }
+
+            } while (preguntasHechasEnTotal < 3);
+
+            Console.WriteLine("-----------------------------------------------------------------------------");
+
+            Console.WriteLine("La cantidad de mujeres es: " + cantidadMujeres);
+            Console.WriteLine("La cantidad de Hombres es: " + cantidadHombres);
+            Console.WriteLine("La cantidad de Mayores de edad es: " + mayorDeEdad);
+            Console.WriteLine("La cantidad de Menores de edad es: " + menorDeEdad);
+
+        }
+    }
+}
